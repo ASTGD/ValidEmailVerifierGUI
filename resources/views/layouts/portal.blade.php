1df8d1685cbb4d89c0e6fd3be0039e380fa84706
Sharwat Shafin <sharwatislam@gmail.com>
2026-01-07 18:38:14 +0600

    fix: load livewire assets in portal layout

--- resources/views/layouts/portal.blade.php
+++ resources/views/layouts/portal.blade.php
@@ -16,9 +16,11 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
+        @livewireStyles
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased bg-gray-100 text-gray-900">
         {{ $slot }}
+        @livewireScripts
     </body>
 </html>
